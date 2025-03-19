5de0e0f96491d30d345099f4d18b6cd192d6ebf3
fabio-k <fabiokmain@gmail.com>
2025-03-19 17:38:15 -0300

    first commit

---
+++ src/main/java/com/muralis/sistemaGerenciamentoContantos/SistemaGerenciamentoContantosApplication.java
@@ -0,0 +1,13 @@
+package com.muralis.sistemaGerenciamentoContantos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciamentoContantosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciamentoContantosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muralis/sistemaGerenciamentoContantos/SistemaGerenciamentoContantosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muralis.sistemaGerenciamentoContantos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciamentoContantosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
