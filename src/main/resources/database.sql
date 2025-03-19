26768821387cf1b3f46c15ac092a9c076c678f49
fabio-k <fabiokmain@gmail.com>
2025-03-19 18:41:16 -0300

    feat: Add database file

---
+++ src/main/resources/database.sql
@@ -0,0 +1,17 @@
+CREATE TABLE cliente (
+    cli_id SERIAL NOT NULL PRIMARY KEY;
+    cli_nome VARCHAR(100) NOT NULL;
+    cli_cpf VARCHAR(11) NOT NULL;
+    cli_data_nascimento DATE NOT NULL;
+    cli_endereco VARCHAR(255)
+);
+
+CREATE TABLE contato (
+    con_id SERIAL NOT NULL PRIMARY KEY;
+    con_cli_id INT NOT NULL;
+    con_tipo VARCHAR(20) NOT NULL;
+    con_valor VARCHAR(100) NOT NULL;
+    con_observacao VARCHAR(255)
+);
+
+CONSTRAINT fk_con_cli FOREIGN KEY (con_cli_id) REFERENCES cliente(cli_id) ON DELETE CASCADE;
