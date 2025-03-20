84bd8f4052b8b9c47bd1b85295363797efbe3b92
fabio-k <fabiokmain@gmail.com>
2025-03-20 11:30:28 -0300

    fix: remove semicolons on the atributes and add endereco table

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,17 +1,27 @@
 CREATE TABLE cliente (
-    cli_id SERIAL NOT NULL PRIMARY KEY;
-    cli_nome VARCHAR(100) NOT NULL;
-    cli_cpf VARCHAR(11) NOT NULL;
-    cli_data_nascimento DATE NOT NULL;
-    cli_endereco VARCHAR(255)
+    cli_id SERIAL NOT NULL PRIMARY KEY,
+    cli_nome VARCHAR(100) NOT NULL,
+    cli_cpf VARCHAR(11) NOT NULL UNIQUE,
+    cli_data_nascimento DATE NOT NULL
+);
+
+CREATE TABLE endereco (
+    end_id SERIAL NOT NULL PRIMARY KEY,
+    end_cli_id INT NOT NULL,
+    end_rua VARCHAR(30),
+    end_bairro VARCHAR(30),
+    end_cidade VARCHAR(30),
+    end_estado VARCHAR(30),
+    end_pais VARCHAR(30),
+    CONSTRAINT fk_end_cli FOREIGN KEY(end_cli_id) REFERENCES cliente(cli_id) ON DELETE CASCADE
 );
 
 CREATE TABLE contato (
-    con_id SERIAL NOT NULL PRIMARY KEY;
-    con_cli_id INT NOT NULL;
-    con_tipo VARCHAR(20) NOT NULL;
-    con_valor VARCHAR(100) NOT NULL;
-    con_observacao VARCHAR(255)
+    con_id SERIAL NOT NULL PRIMARY KEY,
+    con_cli_id INT NOT NULL,
+    con_tipo VARCHAR(20) NOT NULL,
+    con_valor VARCHAR(100) NOT NULL,
+    con_observacao VARCHAR(255),
+    CONSTRAINT fk_con_cli FOREIGN KEY (con_cli_id) REFERENCES cliente(cli_id) ON DELETE CASCADE
 );
 
-CONSTRAINT fk_con_cli FOREIGN KEY (con_cli_id) REFERENCES cliente(cli_id) ON DELETE CASCADE;
